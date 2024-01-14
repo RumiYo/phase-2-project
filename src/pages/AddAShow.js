c4db27c265742236a234e2daf6487f9f45d308b0
RumiYo <yokoyamarumi@gmail.com>
2024-01-14 03:43:41 -0500

    Created newShow data

--- src/pages/AddAShow.js
+++ src/pages/AddAShow.js
@@ -7,14 +7,12 @@ function AddAShow(){
 
     const [nameNew, setNameNew] = useState('');
     const [ratingNew, setRatingNew] = useState('');
+    const [premieredNew, setPremieredNew] = useState('');
     const [genresNew, setGenresNew] = useState([]);
     const [networkNew, setNetworkNew] = useState('');
     const [countryNew, setCountryNew] = useState('');
     const [imageUrlNew, setImageUrlNew] = useState(''); 
     const [summaryNew, setSummaryNew] = useState('');
-
-
-
 
     function checkboxSelect(e){
         let selectedGenres = genresNew
@@ -25,13 +23,21 @@ function AddAShow(){
             selectedGenres = selectedGenres.filter(genre => genre!==e.target.value)
             }
             setGenresNew(selectedGenres)
-            console.log(genresNew)
         }
 
 
     function submitClick(e){
         e.preventDefault();
-        console.log("clicked", genresNew )
+        const newShow = {
+            name: nameNew,
+            genres: genresNew,
+            rating:{average: parseInt(ratingNew)},
+            network:{name: networkNew, country:{name: countryNew,}},
+            image:{original: imageUrlNew},
+            summary: summaryNew,
+            premiered: premieredNew,
+        }
+        console.log(newShow)
     }
 
     return (
@@ -39,10 +45,13 @@ function AddAShow(){
             <h1>Add show</h1>
             <form id="inputForm">
                 <label className="inputLabel" for="name">Show title: </label>
-                <input type="text" id="name"  size="40" onChange={ e => setNameNew(e.target.value) } required/><br/>
+                <input type="text" id="name" placeholder="Stranger Things" size="40" onChange={ e => setNameNew(e.target.value) } required/><br/>
 
                 <label  className="inputLabel" for="rating">Rating:   </label>
-                <input type="number" min="0.1" max="10.0" step="0.1" id="rating" onChange={ e => setRatingNew(e.target.value) } required/><br/>
+                <input type="number" min="0.1" max="10.0" step="0.1" id="rating" placeholder="8.9" onChange={ e => setRatingNew(e.target.value) } required/><br/>
+
+                <label  className="inputLabel" for="premiered">Premiered:   </label>
+                <input type="date" id="premiered" placeholder="" onChange={ e => setPremieredNew(e.target.value) } required/><br/>
 
                 <label className="inputLabel" for="genres">Genres: </label>
                 <div id="genresSelect" >
@@ -55,16 +64,16 @@ function AddAShow(){
                 </div><br/>
 
                 <label className="inputLabel" for="network">Network: </label>
-                <input type="text" id="network"   size="30" onChange={ e => setNetworkNew(e.target.value) } required /><br/>
+                <input type="text" id="network" placeholder="Netflix"  size="30" onChange={ e => setNetworkNew(e.target.value) } required /><br/>
 
                 <label className="inputLabel" for="country">Country: </label>
-                <input type="text" id="country" onChange={ e => setCountryNew(e.target.value) } required /><br/>
+                <input type="text" id="country" placeholder="United States" onChange={ e => setCountryNew(e.target.value) } required /><br/>
 
                 <label className="inputLabel" for="imageUrl">Image URL: </label>
-                <input type="text" id="imageUrl" size="60" onChange={ e => setImageUrlNew(e.target.value) } required /><br/>
+                <input type="url" pattern="https://.*" placeholder="https://example.com" id="imageUrl" size="60" onChange={ e => setImageUrlNew(e.target.value) } required /><br/>
 
                 <label className="inputLabel" for="summary">Summary: </label>
-                <textarea id="summary" rows="7" cols="100"  onChange={ e => setSummaryNew(e.target.value) } required /><br/>
+                <textarea id="summary" rows="7" cols="100" placeholder="When a young boy vanishes, a small town uncovers a mystery involving secret experiments, terrifying supernatural forces and one strange little girl." onChange={ e => setSummaryNew(e.target.value) } required /><br/>
 
                 <button id="addButton" type="submit" value="Add a new show" onClick={submitClick}>Add a new show</button>        
                 
