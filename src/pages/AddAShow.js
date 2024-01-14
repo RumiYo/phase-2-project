88b33d7fcfd220a25ef08c7179375c1e99d7e287
RumiYo <yokoyamarumi@gmail.com>
2024-01-14 02:02:17 -0500

    Added submit button on AddAShow component

--- src/pages/AddAShow.js
+++ src/pages/AddAShow.js
@@ -11,8 +11,10 @@ function AddAShow(){
             <form id="inputForm">
                 <label className="inputLabel" for="name">Show title: </label>
                 <input type="text" id="name"  size="40" required/><br/>
+
                 <label  className="inputLabel" for="rating">Rating:   </label>
-                <input type="text" id="rating" required/><br/>
+                <input type="number" min="0.1" max="10.0" step="0.1" id="rating" required/><br/>
+
                 <label className="inputLabel" for="genres">Genres: </label>
                 <div id="genresSelect" >
                     {genres.map(genre => (
@@ -22,16 +24,20 @@ function AddAShow(){
                         </span>
                     ))}
                 </div><br/>
+
                 <label className="inputLabel" for="network">Network: </label>
                 <input type="text" id="network"   size="40" required /><br/>
+
                 <label className="inputLabel" for="country">Country: </label>
                 <input type="text" id="country" required /><br/>
+
                 <label className="inputLabel" for="imageUrl">Image URL: </label>
                 <input type="text" id="imageUrl"   size="60" required /><br/>
+
                 <label className="inputLabel" for="summary">Summary: </label>
                 <textarea id="summary" rows="7" cols="100" required /><br/>
 
-
+                <button id="addButton" type="submit" Value="Add a new show">Add a new show</button>        
                 
 
             </form>
